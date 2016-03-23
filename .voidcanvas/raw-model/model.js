3d81a31a8d9b9cde3e76fad560faf235835e5a15
metalshan <paul@voidcanvas.com>
2016-03-23 20:44:00 +0530

    raw model

---
+++ .voidcanvas/raw-model/model.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+let BaseModel = localrequire('baseModel');
+let validationConfig = require('./validations.json');
+let propertiesConfig = require('./properties.json');
+
+class @modelName@ extends BaseModel{
+
+	//the constructor of the model. 
+	constructor(obj){
+		super(obj);
+
+		//model specific customizations can be done here
+	}
+
+	getProperties(){
+		return propertiesConfig;
+	}
+
+	getValidations(){
+		return validationConfig;	
+	} 
+}
+
+module.exports = @modelName@;
